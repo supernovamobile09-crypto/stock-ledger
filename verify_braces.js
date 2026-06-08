b98945bc8a0707b34a135bfd704023d3fab02be0
Supernova <supernovamobile09@gmail.com>
2026-06-08 22:08:14 +0400

    chore: remove temp debug scripts

--- verify_braces.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('index (MASTER 2).html', 'utf8');
-let block = c.substring(613380, 621251); // function syncPLMTransactions body
-let open = 0, close = 0;
-for (let ch of block) { if (ch == '{') open++; if (ch == '}') close++; }
-console.log('Body braces - Open:', open, 'Close:', close);
-console.log('Balanced:', open === close);
